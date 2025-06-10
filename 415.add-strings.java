7321040fe54249aafd1b64062a51ef1fae274a3b
XinNing0 <ningellenx@gmail.com>
2025-06-10 19:35:50 -0400

    add string

---
+++ 415.add-strings.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=415 lang=java
+ *
+ * [415] Add Strings
+ */
+
+// @lc code=start
+class Solution {
+    public String addStrings(String num1, String num2) {
+        return addStringsHelper(num1, num2);
+    }
+
+    private String addStringsHelper(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += num2.charAt(j--) - '0';
+            }
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString();
+    }
+}
+// @lc code=end
+
